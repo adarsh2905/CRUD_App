f61a02e48f827fec9b09a253c79e1cf166df0d7d
adarsh.k <adarsh.k@TL308>
2022-08-22 14:42:25 +0530

    initial commit

---
+++ CrudApp/Data.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CrudApp
+{
+    public class Data
+    {
+        public static int CreateUser(string userName, int userAge, SqlConnection sqlConnection)
+        {
+            string insertQuery = "INSERT INTO DETAILS(user_Name, user_Age) VALUES('" + userName + "'," + userAge + "); select SCOPE_IDENTITY();";
+            SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
+            int id = Convert.ToInt32(insertCommand.ExecuteScalar());
+            return id;
+           
+        }
+
+        public static List<User> RetrieveUser(SqlConnection sqlConnection)
+        {
+            List<User> users = new List<User>();
+            string displayQuery = "SELECT * FROM Details";
+            SqlCommand displayCommand = new SqlCommand(displayQuery, sqlConnection);
+            SqlDataReader dataReader = displayCommand.ExecuteReader();
+            while (dataReader.Read())
+            {
+                users.Add(new User
+                {
+                    UserId = Convert.ToInt32(dataReader["user_id"]),
+                    UserName = dataReader["user_name"].ToString(),
+                    UserAge = Convert.ToInt32(dataReader["user_age"])
+                }) ;
+            }
+            dataReader.Close();
+            return users;
+        }
+
+        public static bool UpdateUser(int age, int id, SqlConnection sqlConnection)
+        {
+            string updateQuery = "UPDATE Details SET user_age = " + age + "WHERE user_id = " + id + "";
+            SqlCommand updateCommand = new SqlCommand(updateQuery, sqlConnection);
+            int updatedId = updateCommand.ExecuteNonQuery();
+            bool updated = false;
+            if (updatedId > 0)
+                updated = true;
+            return updated;
+        }
+
+        public static bool DeleteUser(int id, SqlConnection sqlConnection)
+        {
+            string deleteQuery = "DELETE FROM DETAILS WHERE user_id = " + id;
+            SqlCommand deleteCommand = new SqlCommand(deleteQuery, sqlConnection);
+            int deletedId = deleteCommand.ExecuteNonQuery();
+            bool deleted = false;
+            if (deletedId > 0)
+                deleted = true;
+            return deleted;
+        }
+    }
+}
--- CrudApp/Program.cs
+++ CrudApp/Program.cs
@@ -1,88 +1,88 @@
-﻿using System.Data.SqlClient;
-// See https://aka.ms/new-console-template for more information
-string connectionString = @"Data Source=LAPTOP-ENJ0A7G1;Initial Catalog=CoDb;Integrated Security=True";
-SqlConnection sqlConnection = new SqlConnection(connectionString);
-sqlConnection.Open();
+﻿using CrudApp;
+using System.Data.SqlClient;
+using System.Xml.Serialization;
 
-try
+
+public class Program
 {
-    Console.WriteLine("Connection established successfully!");
-    string answer;
-    do
+
+
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Select the operation to perform: \n1. Creation\n2. Retrieve\n3. Updation\n4. Deletion ");
+        string connectionString = @"Data Source=TL308\SQLEXPRESS;Initial Catalog=CoDb;Integrated Security=True";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+        sqlConnection.Open();
 
-        int choice = int.Parse(Console.ReadLine());
-
-        switch (choice)
+        try
         {
-            case 1:
-                //CREATE operation
-                Console.WriteLine("Enter your name:");
-                string userName = Console.ReadLine();
-                Console.WriteLine("Enter your age:");
-                int userAge = int.Parse(Console.ReadLine());
-                string insertQuery = "INSERT INTO DETAILS(user_Name, user_Age) VALUES('" + userName + "'," + userAge + ")";
-                SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
-                insertCommand.ExecuteNonQuery();
-                Console.WriteLine("Data is successfully inserted into table !!");
-                break;
+            Console.WriteLine("Connection established successfully!");
+            string answer;
+            do
+            {
+                Console.WriteLine("Select the operation to perform: \n1. Creation\n2. Retrieve\n3. Updation\n4. Deletion ");
 
-            case 2:
-                //RETRIEVE operation
-                string displayQuery = "SELECT * FROM Details";
-                SqlCommand displayCommand = new SqlCommand(displayQuery, sqlConnection);
-                SqlDataReader dataReader = displayCommand.ExecuteReader();
-                while (dataReader.Read())
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
                 {
-                    Console.WriteLine("Id: " + dataReader.GetValue(0).ToString());
-                    Console.WriteLine("Name: " + dataReader.GetValue(1).ToString());
-                    Console.WriteLine("Age: " + dataReader.GetValue(2).ToString());
+                    case 1:
+                        //CREATE operation
+                        Console.WriteLine("Enter your name:");
+                        string userName = Console.ReadLine();
+                        Console.WriteLine("Enter your age:");
+                        int userAge = int.Parse(Console.ReadLine());
+                        int new_id = Data.CreateUser(userName, userAge, sqlConnection);
+                        Console.WriteLine("Data inserted successfully. ID of inserted record: " + new_id);
+                        break;
+
+                    case 2:
+                        //RETRIEVE operation
+                        List<User> users = Data.RetrieveUser(sqlConnection);
+                        for(int i = 0; i < users.Count; i++)
+                        {
+                            Console.WriteLine("ID: "+users[i].UserId);
+                            Console.WriteLine("Name: " + users[i].UserName);
+                            Console.WriteLine("Age: " + users[i].UserAge);
+                        }
+                        break;
+
+                    case 3:
+                        //UPDATE operation
+                        Console.WriteLine("Enter the user id that you would like to update:");
+                        int u_id = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the age of the user to update");
+                        int u_age = int.Parse(Console.ReadLine());
+                        Data.UpdateUser(u_age, u_id, sqlConnection);
+
+                        break;
+
+                    case 4:
+                        //DELETE operation
+                        Console.WriteLine("Enter the id of the record to be deleted !");
+                        int d_id = int.Parse(Console.ReadLine());
+                        Console.WriteLine(Data.DeleteUser(d_id, sqlConnection));
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+
                 }
-                dataReader.Close();
-                break;
+                Console.WriteLine("Do you want to continue ?");
+                answer = Console.ReadLine();
 
-            case 3:
-                //UPDATE operation
-                int u_id;
-                int u_age;
-                Console.WriteLine("Enter the user id that you would like to update:");
-                u_id = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter the age of the user to update");
-                u_age = int.Parse(Console.ReadLine());
-                string updateQuery = "UPDATE Details SET user_age = " + u_age + "WHERE user_id = " + u_id + "";
-                SqlCommand updateCommand = new SqlCommand(updateQuery, sqlConnection);
-                updateCommand.ExecuteNonQuery();
-                Console.WriteLine("Data updated successfully !");
-                break;
-
-            case 4:
-                //DELETE operation
-                Console.WriteLine("Enter the id of the record to be deleted !");
-                int d_id = int.Parse(Console.ReadLine());
-                string deleteQuery = "DELETE FROM DETAILS WHERE user_id = " + d_id;
-                SqlCommand deleteCommand = new SqlCommand(deleteQuery, sqlConnection);
-                deleteCommand.ExecuteNonQuery();
-                Console.WriteLine("Deletion successful !");
-                break;
-
-            default:
-                Console.WriteLine("Invalid input");
-                break;
+            } while (answer == "y" || answer == "Y"|| answer == "yes" || answer == "Yes");
 
         }
-        Console.WriteLine("Do you want to continue ?");
-        answer = Console.ReadLine();
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
 
-    } while (answer != "No");
-
-}
-catch (Exception e)
-{
-    Console.WriteLine(e.Message);
-}
-
-finally
-{
-    sqlConnection.Close();
+        finally
+        {
+            sqlConnection.Close();
+        }
+    }
+    // See https://aka.ms/new-console-template for more info
 }
